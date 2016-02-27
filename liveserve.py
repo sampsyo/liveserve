4d6b6719d56a5aa020d1d07d2e7a806475b4fe33
Adrian Sampson <adrian@radbox.org>
2016-02-26 16:23:15 -0800

    Initial script sketch

---
+++ liveserve.py
@@ -0,0 +1,35 @@
+import click
+import livereload
+
+
+@click.command()
+@click.option('--host', '-h', default='127.0.0.1',
+              help='Hostname to run the server on')
+@click.option('--port', '-p', default=35729,
+              help='Port to run the server on')
+@click.argument('servedir', default='.',
+                type=click.Path(exists=True, dir_okay=True, file_okay=False))
+@click.option('--watch', '-w', multiple=True, type=click.Path(),
+              help='File or directory to watch')
+@click.option('--command', '-c',
+              help='Shell command to run on change')
+def liveserve(host, port, servedir, watch, command):
+    """Run a LiveReload HTTP server.
+    """
+    # If the user doesn't give us any explicit files to watch, watch the
+    # served directory.
+    if not watch:
+        watch = (servedir,)
+
+    # Set up and run the server.
+    server = livereload.Server()
+    for path in watch:
+        if command:
+            server.watcher.watch(path, livereload.shell(command))
+        else:
+            server.watcher.watch(path)
+    server.serve(host=host, port=port, root=servedir)
+
+
+if __name__ == '__main__':
+    liveserve()
